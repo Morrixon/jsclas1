fdb2fa4d7c6ede2ce5ae22a54a844e4b821ce989
morri <morrisoneano@gmail.com>
2022-06-26 22:10:39 -0300

    1ro

---
+++ index.js
@@ -0,0 +1,5 @@
+let numero1 = parseInt(prompt("ingrese el numero 1"));
+let numero2 = parseInt(prompt("ingrese el numero 2"));
+let suma = numero1 + pareIntnumero2;
+alert("la suma de los dos numeros es " + suma);
+
